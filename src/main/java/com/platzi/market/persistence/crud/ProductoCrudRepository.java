9486e5665ff281fc8ff8c23f5ba546f9775a22ce
ItzelAlonsoG <itzel.alonso@accenture.com>
2021-07-13 16:32:43 -0500

    implementacion query methods

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,19 @@ package com.platzi.market.persistence.crud;
 import com.platzi.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 // la extension Crud solo necesita el nombre de la clase (tabla) y el tipo de dato de la llame primaria.
 public interface ProductoCrudRepository extends CrudRepository <Producto,Integer> {
+    /* query de manera nativa
+    @Query(value = "SELECT * FROM producos WHERE id_categoria =?", nativeQuery = true)
+    List<Producto> getByCategoria(int idCategoria);
+    */
+
+    //querymethod
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
 }
